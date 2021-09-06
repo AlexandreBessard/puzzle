87186a947a305598459e31e4ca93796adc196d10
AlexandreBessard <alexandre.bessard@gmail.com>
2021-09-06 17:24:46 +0200

    sign in page added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import HomePage from './pages/homepage/homepage.component';
 import {Switch, Route} from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignOutPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.compenent';
 
 import './App.css';
 
@@ -15,6 +16,7 @@ function App() {
       <Route exact path='/' component={HomePage} />
       <Route exact path='/puzzle/' component={HomePage} />
       <Route exact path='/puzzle/shop/' component={ShopPage} />
+      <Route exact path='/puzzle/signIn/' component={SignInAndSignOutPage} />
       </Switch>
     </div>
   );
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -18,6 +18,9 @@ const Header = () => (
       <Link className='option' to='/puzzle/'>
         CONTACT
       </Link>
+      <Link className='option' to='/puzzle/signIn'>
+        SIGN IN
+      </Link>
     </div>
   </div>
 );
