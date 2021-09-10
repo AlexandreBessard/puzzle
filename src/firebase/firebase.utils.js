4d5813e0586784867be8dd9d0a0abcdea20a3a4b
AlexandreBessard <alexandre.bessard@gmail.com>
2021-09-10 18:11:05 +0200

    firebase version 8.9.1

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,12 +1,7 @@
-import firebase from 'firebase/app';
+import firebase from 'firebase/app'
 import 'firebase/firestore';
 import 'firebase/auth';
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const config = {
   apiKey: "AIzaSyBNT_y31P_M7ti7cij2MMgCMcn8vGAMy58",
   authDomain: "crwn-db-433c1.firebaseapp.com",
@@ -16,6 +11,7 @@ const config = {
   appId: "1:43081720070:web:0e1fbb58f52727c097fe87",
   measurementId: "G-6J033VNZMC"
 };
+
 
 firebase.initializeApp(config);
 
