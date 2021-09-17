abb388a5f1ca064719020bcfa42d0f2d2f0fc6bf
AlexandreBessard <alexandre.bessard@gmail.com>
2021-09-17 13:34:37 +0200

    checkout page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-import HomePage from './pages/homepage/homepage.component';
-import {Switch, Route, Redirect} from 'react-router-dom';
-import ShopPage from './pages/shop/shop.component';
-import Header from './components/header/header.component';
-import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-
+  
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { setCurrentUser } from './redux/user/user.actions';
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { createStructuredSelector } from 'reselect';
 
 import './App.css';
-import React from 'react';
+
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import CheckoutPage from './pages/checkout/checkout.component';
+
+import Header from './components/header/header.component';
+
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+
+import { setCurrentUser } from './redux/user/user.actions';
+import { selectCurrentUser } from './redux/user/user.selectors';
 
 
 class App extends React.Component {
@@ -50,6 +57,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/puzzle' component={HomePage} />
           <Route path='/puzzle/shop/' component={ShopPage} />
+          <Route exact path='/checkout' component={CheckoutPage} />
           <Route
             exact
             path='/puzzle/signIn/'
@@ -67,8 +75,8 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
 })
 
 const mapDispatchToProps = dispatch => ({
--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -1,20 +1,33 @@
-import { createSelector } from "reselect";
+import { createSelector } from 'reselect';
 
-//Input selector or Output selector
-//Input
 const selectCart = state => state.cart;
 
-//Rely on selectCart
 export const selectCartItems = createSelector(
-    [selectCart],
-    (cart) => cart.cartItems
+  [selectCart],
+  cart => cart.cartItems
 );
 
-//Rely on selectCartItems
+export const selectCartHidden = createSelector(
+  [selectCart],
+  cart => cart.hidden
+);
+
 export const selectCartItemsCount = createSelector(
-    [selectCartItems],
-    cartItems => 
+  [selectCartItems],
+  cartItems =>
     cartItems.reduce(
-        (accumulatedQuantity, cartItem) => accumulatedQuantity + cartItem.quantity, 0
+      (accumalatedQuantity, cartItem) =>
+        accumalatedQuantity + cartItem.quantity,
+      0
+    )
+);
+
+export const selectCartTotal = createSelector(
+  [selectCartItems],
+  cartItems =>
+    cartItems.reduce(
+      (accumalatedQuantity, cartItem) =>
+        accumalatedQuantity + cartItem.quantity * cartItem.price,
+      0
     )
 );
